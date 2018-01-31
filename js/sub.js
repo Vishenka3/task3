f22b36d414c23e949b71e08bac113e6a638b5970
Alex <djdoktop@gmail.com>
2018-02-01 01:38:21 +0300

    more accurate geolocation

--- js/sub.js
+++ js/sub.js
@@ -1,33 +1,40 @@
 $(document).ready(function() {
 
     ymaps.ready(function() {
-        var country = ymaps.geolocation.country;
+        /*var country = ymaps.geolocation.country;
         var city = ymaps.geolocation.city;
-
+        $('#country').html('Ваша страна: '+country);
+        $('#city').html('Ваш город: '+city);*/
         var myMap = new ymaps.Map("map", {
             center: [52.111406,26.102473],
-            zoom: 5
+            zoom: 3
         });
-
-        $('#country').html('Ваша страна: '+country);
-        $('#city').html('Ваш город: '+city);
+        myMap.controls.add(
+            new ymaps.control.ZoomControl()
+        );
 
         $("#submit").click(function () {
             var cityName = $("#cityName").val();
             console.log(cityName);
 
-            var myGeocoder = ymaps.geocode(cityName);
+            var myGeocoder = ymaps.geocode(cityName, {results: 1, kind: 'locality'});
             myGeocoder.then(
                 function (res) {
-                    console.log('Координаты объекта :' + res.geoObjects.get(0).geometry.getCoordinates());
-                    var coords = res.geoObjects.get(0).geometry.getCoordinates();
-                    myMap.geoObjects.add(
-                        new ymaps.Placemark(coords,
-                            {iconContent: ''},
-                            {preset: 'twirl#redStretchyIcon'}
-                        )
-                    );
-
+                    if(res.geoObjects.get(0)) {
+                        var coords = res.geoObjects.get(0).geometry.getCoordinates();
+                        console.log(res.geoObjects.get(0).properties._k);
+                        myMap.setCenter(coords, 7);
+                        myMap.geoObjects.add(
+                            new ymaps.Placemark(coords,
+                                {iconContent: ''},
+                                {preset: 'twirl#redStretchyIcon'}
+                            )
+                        );
+                        console.log('Координаты объекта :' + res.geoObjects.get(0).geometry.getCoordinates());
+                    }else{
+                        alert('Такого города нет');
+                    }
+                    //
                 },
                 function (err) {
                     console.log('Ошибка');
@@ -35,7 +42,6 @@ $(document).ready(function() {
             );
         });
     });
-
 
 });
 
