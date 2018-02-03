331cf4057507a032631bffb30daa9bba58a4e375
Alex <djdoktop@gmail.com>
2018-02-04 01:36:29 +0300

    fun with db

--- js/sub.js
+++ js/sub.js
@@ -15,8 +15,11 @@ $(document).ready(function() {
 
         $("#submit").click(function () {
             var cityName = $("#cityName").val();
-            console.log(cityName);
+            locate(cityName);
 
+        });
+
+        function locate(cityName) {
             var myGeocoder = ymaps.geocode(cityName, {results: 1, kind: 'locality'});
             myGeocoder.then(
                 function (res) {
@@ -40,7 +43,60 @@ $(document).ready(function() {
                     console.log('Ошибка');
                 }
             );
+        }
+
+        $("#reset").click(function () {
+            console.time('test callback');
+            var cityName ="\""+ $("#cityName").val() + "\"";
+            var file = document.getElementById('fileItem').files[0];
+            var results = [];
+            var k=0;
+            var config = {
+                delimiter: ",",	// auto-detect
+                newline: "\r",	// auto-detect
+                quoteChar: '"',
+                header: true,
+                dynamicTyping: false,
+                preview: 0,
+                encoding: "",
+                worker: false,
+                comments: false,
+                step: function(row) {
+                    results.push(row.data);
+                    if(cityName.localeCompare(row.data[0].title_ru) === 0)  k=1;
+                },
+                complete: function() {
+                    console.log("Количество: " + results.length);
+                    k===1 ?  console.log("Нашелся") : console.log("He нашелся");
+                    k=0;
+                    console.log("results " + results[4][0].title_ru);
+                    console.timeEnd('test callback');
+                    locate(cityName);
+                },
+                error: function(error) {
+                    console.log(error);
+                },
+                download: false,
+                skipEmptyLines: false,
+                chunk: undefined,
+                fastMode: true,
+                beforeFirstChunk: undefined,
+                withCredentials: undefined
+            };
+            Papa.parse(file, config);
+            //console.log("results " + results);
+            /*Papa.parse(file,{
+                header: true,
+                delimiter: ",",
+                step: function(row) {
+                    console.log("Row:", row.data);
+                },
+                complete: function() {
+                    console.log();
+                }
+            });*/
         });
+
     });
 
 });
